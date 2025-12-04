5941876332bfc5ee70486c1e6e06bbf4b785513e
Eric <ericjohn121709@gmail.com>
2025-12-04 10:06:15 +0800

    app name and icon update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'MosesIonic',
+  appName: 'Project Genesis',
   webDir: 'dist',
   server: {
     cleartext: true
